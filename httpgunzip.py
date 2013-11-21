4f2d5ec978b13fdf77a81bba85d60cdbcb660267
Brian Carcich <drbitboy@gmail.com>
2013-11-21 11:00:53 -0500

    Simplified httpgunzip.py (no more recursion); added comments

--- httpgunzip.py
+++ httpgunzip.py
@@ -1,68 +1,143 @@
+"""
+httpgunzip.py - Get large GZIPped files from FTP server, including those
+                that are GZIPped in pieces e.g. tyc2.dat as
+                tyc2.dat.00.gz, tyc2.01.gz, ...
+
+Usage:  python httpgunzip.py index.dat suppl_1.dat 0 tyc2.dat ReadMe
+
+N.B. This script was created to download Tycho-2 star catalog and index
+     files from Univ. Strasburg in France; the httpgunzip class can do
+     other constructs; see the constructor documentation.
+
+"""
+import re
+import os
 import sys
 import zlib
 import urllib2
 
+
+########################################################################
+def checkNNgz(fnIn, line):
+  """
+  Inputs:  base filename; line from FTP listing
+
+  Return filename match from line matching blah.dat as blah.dat.gz or
+  blah.dat.NN.gz, or return None
+
+  """
+  reObj = re.search( '(^|[\W])%s([.]\d\d)?([.]gz$)' % (fnIn,) ,line)
+  if reObj:
+    return ''.join( [i for i in reObj.groups()[1:] if not i is None])
+  return None
+
+
+########################################################################
+### Default FTP URL is University of Strasburg, to get Tycho-2 data
+### catalog and index files
+
+urlPfxDefault = 'ftp://cdsarc.u-strasbg.fr/cats/I/259/'
+
 class httpgunzip:
-  def __init__(self, fnUrl, fnOut, initial=None):
-    self.fnUrl = fnUrl
-    self.fnOut = fnOut
-    self.initial = initial
-    self.fmt = 'ftp://cdsarc.u-strasbg.fr/cats/I/259/%s%s.gz'
+  """
+  Class that implments, for one final file, the following pipe:
 
-  def go(self,initial):
-    if initial is None: mode,mfx = 'wb', ''
-    elif not initial  : mode,mfx = 'wb', '.00'
-    else              : mode,mfx = 'ab', '.%02d' % (initial,)
+    [FTP URL GET] | [Decompression] | [Write file]
 
-    try:
-      fn = self.fmt % (self.fnUrl,mfx,)
-      sys.stderr.write( 'Starting %s ...\n' % (fn,) )
+  Constructor:  httpgunzip(fnUrlFn, pathOut, urlPfx)
+
+  Arguments:
+
+    fnUrlFn:  basename of file, without .NN or .gz suffixes
+
+    pathOut:  path of output file to be written
+
+    urlPfx:  URL to directory containing file
+             - defaults to Univ. Strasburg Tycho2 files
+
+  N.B. File outputFilename will be overwritten
+
+  """
+  def __init__(self, fnUrlFn, pathOut, urlPfx=urlPfxDefault):
+    self.fnUrlFn = fnUrlFn
+    self.pathOut = pathOut
+    self.urlPfx = urlPfx
+    ### Get lines of FTP listing
+    self.lines = urllib2.urlopen(self.urlPfx).read().split('\n')
+
+  def go(self,testOnly=True):
+    """
+    Routine to decompress all .gz files matching self.fnUrlFn into
+    output file
+
+    """
+    mode = 'wb'
+    for line in self.lines:
+      line = line.strip('\r')
+      sfx = checkNNgz(self.fnUrlFn, line)
+      if sfx is None: continue
+      if mode == 'wb':
+        sys.stderr.write( 'Starting %s ...\n' % (self.pathOut,) )
+      
+      url = os.path.join(self.urlPfx,self.fnUrlFn+sfx)
+      sys.stderr.write( '  %s\n' % (url,) )
       sys.stderr.flush()
-      self.fZin = urllib2.urlopen(fn)
-    except:
-      import traceback
-      traceback.print_exc()
-      return None
 
-    self.fOut = open(self.fnOut,mode)
-    dco = zlib.decompressobj(15+32)
+      if testOnly:
+        mode = 'ab'
+        continue
 
-    while True:
-      dada = self.fZin.read( 10000 )
-      if not dada: break
-      self.fOut.write( dco.decompress(dada) )
+      fOut = open(self.pathOut,mode)
+      mode = 'ab'
 
-    self.fOut.write( dco.flush() )
-    self.fOut.close()
+      dco = zlib.decompressobj(15+32)
 
-    self.fZin, dco = None,None
+      fZin = urllib2.urlopen(url)
 
-    if initial is None: return None
+      while True:
+        dada = fZin.read( 50000 )
+        if not dada: break
+        fOut.write( dco.decompress(dada) )
+ 
+      fOut.write( dco.flush() )
+      fOut.close()
+      fZin, dco = None,None
 
-    return self.go(initial+1)
+    return
 
+
+########################################################################
+### Default script is set up to get index.dat, tyc2.dat, suppl_1 and
+### ReadMe files from Univ. Strasburg
+########################################################################
 
 if __name__ == "__main__" and sys.argv[1:]:
 
+  ### Default path only tests, does not transfer data
+  testOnly = True
+
   va = sys.argv[1:]
   va.reverse()
-  initial = None
+
   while va:
-    fnUrl = va.pop()
-    if fnUrl[-4:] == '.dat':
-      if fnUrl == 'tyc2.dat': fnOut = 'catalog.dat'
-      else                  : fnOut = fnUrl
-      hg = httpgunzip(fnUrl, fnOut, initial=initial)
-      hg.go(initial)
-      initial = None
+    fnUrlFn = va.pop()
+
+    if fnUrlFn == '--doit':
+      testOnly = False
       continue
 
-    initial = 0
-      
-"""
->>> f=urllib2.urlopen('ftp://cdsarc.u-strasbg.fr/cats/I/259/index.dat.gz')
-while >>> while True:
-...   l=f.read(1000)
-...   if not l: break
+    if fnUrlFn == 'ReadMe':
+      sys.stderr.write( 'Getting %s ...\n' % (fnUrlFn,) )
+      if not testOnly:
+        open( 'ReadMe', 'wb').write(
+          urllib2.urlopen(os.path.join(urlPfxDefault,fnUrlFn)).read()
+        )
+      continue
 
-"""
+    if fnUrlFn[-4:] == '.dat':
+      if fnUrlFn == 'tyc2.dat': pathOut = 'catalog.dat'
+      else                    : pathOut = fnUrlFn
+      hg = httpgunzip(fnUrlFn, pathOut)
+      hg.go(testOnly=testOnly)
+      continue
+
