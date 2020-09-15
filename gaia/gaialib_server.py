f732eb3297fd54644d54ee43c6c18fa762fc0130
Brian Carcich <drbitboy@gmail.com>
2020-09-15 18:46:18 +0000

    Add command-line running for gaialib_server.py; adjust debug output for same

--- gaia/gaialib_server.py
+++ gaia/gaialib_server.py
@@ -8,6 +8,7 @@ import traceback as tb
 
 do_debug = 'DEBUG' in os.environ
 do_extra_debug = 'EXTRA_DEBUG' in os.environ
+do_data_debug = 'DATA_DEBUG' in os.environ
 DEFAULT_PORT = int(''.join(['{0:01x}'.format(15&ord(c)) for c in 'gaia']),16)
 
 def handleSIGCHLD(*args):
@@ -17,6 +18,7 @@ def handleSIGCHLD(*args):
       if 0==pid and 0==status: raise ChildProcessError
       if do_debug:
         sys.stderr.write("Child [{0}] exited with status [{1}]\n".format(pid,status))
+        sys.stderr.flush()
     except ChildProcessError as e:
       return
 
@@ -37,16 +39,24 @@ def do_main(gaialightdb,gaiahostname='',port=DEFAULT_PORT):
         try:
           get_gaia_data(gaialightdb,client_sock,client_addr_info)
         except:
-          if do_extra_debug: tb.print_exc()
+          if do_extra_debug:
+            tb.print_exc()
+            sys.stderr.flush()
         exit(0)
       ### Parent closes its copy of the client socket & continues
       client_sock.close()
-      if do_debug: sys.stderr.write("Child {0} forked\n".format(pid))
+      if do_debug:
+        sys.stderr.write("Child {0} forked\n".format(pid))
+        sys.stderr.flush()
   except SystemExit as e: pass
   except:
-    if do_extra_debug: tb.print_exc()
+    if do_extra_debug:
+      tb.print_exc()
+      sys.stderr.flush()
     try:
-      if do_debug: sys.stderr.write('GaiaLib server closing\n')
+      if do_debug:
+        sys.stderr.write('GaiaLib server closing\n')
+        sys.stderr.flush()
       server.close()
     except:pass
 
@@ -86,11 +96,33 @@ ORDER BY gaialight.phot_g_mean_mag ASC
     (m1,himag,ralo,rahi,declo,dechi,) = inputs = struct.unpack(byteorder + '6d',received)
     assert -1.0 == m1
 
-  if do_debug: sys.stderr.write('Inputs:  {0}\n'.format(inputs))
+  if do_debug: sys.stderr.write('Child Inputs:  {0}\n'.format(inputs))
 
   cn = sqlite3.connect(gaialightdb)
   cu = cn.cursor()
   cu.execute(query,dict(himag=himag,ralo=ralo,rahi=rahi,declo=declo,dechi=dechi))
   for row in cu:
-    if do_debug: sys.stderr.write('Output row:  {0}\n'.format(list(row)))
+    if do_data_debug:
+      sys.stderr.write('Child Output row:  {0}\n'.format(list(row)))
+      sys.stderr.flush()
     client_sock.send(struct.pack(byteorder+'I3d',*list(row)))
+
+########################################################################
+### Usage:  python gaialib_server.py gaia_subset.sqlite3 [--host=''] [--port=29073]
+
+if "__main__" == __name__ and sys.argv[1:] and sys.argv[1].endswith('.sqlite3'):
+
+  hostname,portno = '',DEFAULT_PORT
+  for arg in sys.argv[2:]:
+
+    if arg.startswith('--host='):
+      hostname = arg[7:]
+      continue
+
+    if arg.startswith('--port='):
+      portno = int(arg[7:])
+      continue
+
+    assert False,'Invalid command-line argument [{0}]'.format(arg)
+
+  do_main(sys.argv[1],gaiahostname=hostname,port=portno)
